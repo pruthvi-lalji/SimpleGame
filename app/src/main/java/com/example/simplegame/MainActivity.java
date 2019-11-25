13353fb012b01b5f685ef88f68756fe9eba77ca1
pruthvi-lalji <Pruthvi_lalji@hotmail.co.uk>
2019-11-25 17:29:43 +0000

    Making FullScreen and removing the title

--- app/src/main/java/com/example/simplegame/MainActivity.java
+++ app/src/main/java/com/example/simplegame/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.simplegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setting FullScreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Removes title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
     }
 }
